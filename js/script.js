7cb490110efddd32189a54e9bc6906b2d91543c0
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-01 12:24:13 +0700

    Stats & JavaScript Increment

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
 function useScroll() {
-const navbar = document.querySelector('.navbar');
+  const navbar = document.querySelector('.navbar');
 
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    navbar.classList.add('navbar-sticky')
-  } else {
-    navbar.classList.remove('navbar-sticky')
-  }
-})
-
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('navbar-sticky')
+    } else {
+      navbar.classList.remove('navbar-sticky')
+    }
+  })
 }
 
+function incrementStats() {
+  const counterList = document.querySelectorAll('.counter')
+
+  counterList.forEach((counter) => {
+    counter.innetText = 0
+
+    function updateStats() {
+      const dataTarget = +counter.getAttribute('data-target')
+      const counterText = +counter.innerText
+
+      const increment = dataTarget / 200
+
+      if (counterText < dataTarget) {
+        counter.innerText = Math.ceil(counterText + increment)
+        setTimeout(updateStats, 1)
+      } else {
+        counter.innerText = dataTarget
+      }
+    }
+
+    updateStats()
+    })
+}
+
+
+
+// Event Listeners
 document.addEventListener('DOMContentLoaded', useScroll)
+document.addEventListener('DOMContentLoaded', incrementStats)
